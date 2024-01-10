6c6d911bd97e26ca5bbd57c743d1188e05784e1e
erikarosah <erikarosamg@gmail.com>
2024-01-10 18:18:50 -0300

    Use case: edit question

--- src/test/repositories/in-memory-questions-repository.ts
+++ src/test/repositories/in-memory-questions-repository.ts
@@ -13,6 +13,11 @@ export class InMemoryQuestionsRepository implements QuestionRepository {
       this.items.splice(itemIndex, 1)
     }
 
+    async save(question: Question) {
+      const itemIndex = this.items.findIndex((item) => item.id === question.id)
+      this.items[itemIndex] = question
+    }
+
     async findBySlug(slug: string) {
       const question = this.items.find((item) => item.slug.value === slug)
   
